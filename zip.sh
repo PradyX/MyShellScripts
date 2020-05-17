58c8fdaa519292849888be5eff67c1bafd3813b6
PradyX <pradyumnbhushan@gmail.com>
2020-05-17 10:06:44 +0530

    zip_script: improve code

--- zip.sh
+++ zip.sh
@@ -8,15 +8,23 @@ NC="\033[0m" # reset color
 
 printf "${GREEN}" 
 
+SCRIPT=/home/prady/MyScripts
+KERNEL_OUT_DIR=/home/prady/derpfest/kernel/xiaomi/sdm660/out/arch/arm64/boot
+KERNEL_ZIP=/home/prady/kernel-zip
+
+notif_upload(){
+    cd ${SCRIPT} && ./telegram -f ${KERNEL_ZIP}/derpfest_stock_eas_kernel.zip "Compilation done for device jasmine_sprout at $(date +%X) ! Visit https://github.com/PradyX/unified_kernel_sdm660/commits/master for changelogs."
+}
+
 echo "Entering out dir"
-cd /home/prady/derpfest/kernel/xiaomi/sdm660/out/arch/arm64/boot
+cd ${KERNEL_OUT_DIR}
 
 echo "Copying dtb"
-cp -R Image.gz-dtb /home/prady/kernel-zip
+cp -R Image.gz-dtb ${KERNEL_ZIP}
 
 echo "Zipping"
 pwd
-cd /home/prady/kernel-zip
+cd ${KERNEL_ZIP}
 zip -r -D derpfest_stock_eas_kernel.zip *
 printf "${NC}" 
 
@@ -30,12 +38,12 @@ if [ $base = 1 ]
 then
 echo "Uploading"
 pwd 
-cd /home/prady/MyScripts && ./telegram -f /home/prady/kernel-zip/derpfest_stock_eas_kernel.zip "Compilation done for device jasmine_sprout at $(date +%Y%m%d-%H%M)! Visit https://bit.ly/2xEbN1R for changelogs."
+notif_upload
 fi
 
 if [ $base = 0 ]
 then
-nemo /home/prady/kernel-zip/
+nemo ${KERNEL_ZIP}
 exit 
 fi
 
